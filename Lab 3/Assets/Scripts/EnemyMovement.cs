77a15ec6ff37c5fb0de99cf6068b73206db0e130
Colten Berry <berrycj@hendrix.edu>
2025-02-27 21:16:07 -0600

    Added enemy

---
+++ Lab 3/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float speed = 5.0f;
+    private Rigidbody2D rigidbody2D;
+
+    private bool isGoingLeft = true;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("collision!");
+        if (collision.collider.gameObject.name == "Player")
+        {
+            Debug.Log("hit Player!");
+        }
+        else if (collision.collider.gameObject.name != "Ground Layer")
+        {
+            Debug.Log("Hit " + collision.collider.gameObject.name);
+            isGoingLeft = !isGoingLeft;
+        }
+    }
+
+
+
+    void FixedUpdate()
+    {
+        if (isGoingLeft)
+        {
+            Debug.Log("going left");
+            rigidbody2D.linearVelocity = new Vector2(-1 * speed, rigidbody2D.linearVelocity.y);
+
+        }
+        else if (!isGoingLeft)
+        {
+            Debug.Log("Going right");
+            rigidbody2D.linearVelocity = new Vector2(1 * speed, rigidbody2D.linearVelocity.y);
+
+        }
+    }
+}
