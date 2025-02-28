577538cc5872985e6e4bb45f49e6697662f37d3c
Colten Berry <berrycj@hendrix.edu>
2025-02-27 22:45:52 -0600

    Made a map that should work up until wall jump is introduced

---
+++ Lab 3/Assets/Scripts/SpikeBehavior.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class SpikeBehavior : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.gameObject.name == "Player")
+        {
+            Debug.Log("Player Hit Spikes !!!"); //TODO make some kind of hit
+        }
+    }
+}
